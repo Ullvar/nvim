e449c40d035e1e7848266f64a38c4cfcf6c2993b
ulsm <ub@shapemaker.io>
2022-10-17 13:34:59 +0200

    .

---
+++ lua/plugins.lua
@@ -0,0 +1,19 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use "nvim-lua/plenary.nvim"
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+}
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+  use 'folke/tokyonight.nvim'
+end)
+
