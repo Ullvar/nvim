a86b6b578061df34aa36263e9ea2513052187f0a
ulsm <ub@shapemaker.io>
2023-02-21 14:14:02 +0100

    Fix indent for javascript

--- init.lua
+++ init.lua
@@ -10,7 +10,9 @@ require('gitsigns').setup()
 require('nvim-treesitter.configs').setup({
     ensure_installed = "all",
     sync_install = false,
-
+    indent = {
+       enable = false
+    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
