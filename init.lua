3b35199bf4c969cb75ad1e3ab356ec4cb39e7de3
ullvar <ullvarb@gmail.com>
2024-02-01 06:34:36 +0100

    hide fold indent

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ require('nvim-treesitter.configs').setup({
 
 -- FOLDING SETTUP
 require('ufo').setup()
-vim.o.foldcolumn = '1'
+vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
