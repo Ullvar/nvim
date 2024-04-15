3bf9fc76cdfcca5eca46f2273a780b22ba3df5ec
ulsm <ub@shapemaker.io>
2024-04-15 08:51:21 +0200

    Update theme

--- lua/core/plugin_config/theme.lua
+++ lua/core/plugin_config/theme.lua
@@ -1,35 +1,41 @@
-vim.cmd[[colorscheme tokyonight-night]]
-
 require('tokyonight').setup({
-	transparent = true,
-	style = "night",
+  transparent = true,
+  style = "night",
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+  sidebars = { "qf", "vista_kind", "terminal", "packer", "nvimtree" },
 })
 
+vim.cmd [[colorscheme tokyonight-night]]
+
 local hl = function(thing, opts)
-	vim.api.nvim_set_hl(0, thing, opts)
+  vim.api.nvim_set_hl(0, thing, opts)
 end
 
 hl("SignColumn", {
-	bg = "none",
+  bg = "none",
 })
 
 hl("ColorColumn", {
-	ctermbg = 0,
-	bg = "#555555",
+  ctermbg = 0,
+  bg = "#555555",
 })
 
 hl("CursorLineNR", {
-	bg = "None"
+  bg = "None"
 })
 
 hl("Normal", {
-	bg = "none"
+  bg = "none"
 })
 
 hl("LineNr", {
-	fg = "#5eacd3"
+  fg = "#5eacd3"
 })
 
 hl("netrwDir", {
-	fg = "#5eacd3"
+  fg = "#5eacd3"
 })
+
