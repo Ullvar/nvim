2db65889c1f0284ba57ed7227b9e226424fdcdd5
ulsm <ub@shapemaker.io>
2024-05-29 13:45:59 +0200

    add keybinds to quick fix list items

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,7 +17,7 @@ vim.api.nvim_set_keymap('n', '<C-j>', ':wincmd j<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<C-h>', ':wincmd h<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', ':wincmd l<CR>', { silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>b', '<C-W><C-W>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>b', '<C-W><C-W>', { noremap = true })
 
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
@@ -27,7 +27,8 @@ vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
 
 vim.keymap.set('i', '<M-1>', '<Plug>(copilot-next)')
 vim.keymap.set('i', '<M-2>', '<Plug>(copilot-previous)')
-vim.keymap.set("i", "<M-3>", 'copilot#Accept("")', {noremap = true, silent = true, expr = true, replace_keycodes = false })
+vim.keymap.set("i", "<M-3>", 'copilot#Accept("")',
+  { noremap = true, silent = true, expr = true, replace_keycodes = false })
 
 vim.keymap.set('n', '<leader>[', '<cmd>lua Open_File_In_Github()<cr>', opts)
 
@@ -37,36 +38,39 @@ vim.keymap.set('n', '<leader>tf', '<cmd>:NvimTreeFindFile<cr>', {})
 vim.keymap.set('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>', opts)
 
 function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
 function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
-    else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-    end
+  local cw = vim.fn.expand('<cword>')
+  if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+    vim.api.nvim_command('h ' .. cw)
+  elseif vim.api.nvim_eval('coc#rpc#ready()') then
+    vim.fn.CocActionAsync('doHover')
+  else
+    vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+  end
 end
 
-vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', {silent = true, noremap = true, expr = true, replace_keycodes = false})
-vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], {silent = true, noremap = true, expr = true, replace_keycodes = false})
-vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], {silent = true, noremap = true, expr = true, replace_keycodes = false})
-vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
-vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
-vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
-vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
-vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", {silent = true})
-vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
-vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
-vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
-vim.keymap.set("n", "<leader>ac", "<Plug>(coc-codeaction)", {silent = true, nowait = true})
+vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
+  { silent = true, noremap = true, expr = true, replace_keycodes = false })
+vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]],
+  { silent = true, noremap = true, expr = true, replace_keycodes = false })
+vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+  { silent = true, noremap = true, expr = true, replace_keycodes = false })
+vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", { silent = true })
+vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", { silent = true })
+vim.keymap.set("n", "gd", "<Plug>(coc-definition)", { silent = true })
+vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
+vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", { silent = true })
+vim.keymap.set("n", "gr", "<Plug>(coc-references)", { silent = true })
+vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
+vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
+vim.keymap.set("n", "<leader>ac", "<Plug>(coc-codeaction)", { silent = true, nowait = true })
 
 vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
-vim.api.nvim_create_user_command("Fold", "call CocAction('fold', <f-args>)", {nargs = '?'})
+vim.api.nvim_create_user_command("Fold", "call CocAction('fold', <f-args>)", { nargs = '?' })
 vim.api.nvim_create_user_command("OR", "call CocActionAsync('runCommand', 'editor.action.organizeImport')", {})
 vim.api.nvim_create_user_command("RN", "call CocActionAsync('runCommand', 'workspace.renameCurrentFile')", {})
 
@@ -76,3 +80,6 @@ vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document
 vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+vim.keymap.set('n', '<leader>j', '<cmd>:cn<cr>', {})
+vim.keymap.set('n', '<leader>k', '<cmd>:cp<cr>', {})
